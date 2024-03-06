97776c8ecb1182613f65886da874281fab50fabd
babayl0729 <babayl0729@gmail.com>
2024-03-05 18:25:39 -0700

    10x10 Muliplication table

---
+++ TenByTenMultiplicationTable.java
@@ -0,0 +1,25 @@
+/*
+*   The program should not read text from the keyboard.
+*	The program should display text on the screen.
+*	The program should display a 10x10 multiplication table.
+*	The program must use any kind of loop: for, while, or do-while.
+ */
+
+package java_practices;
+
+/* 
+Multiplication table
+*/
+
+public class TenByTenMultiplicationTable {
+    public static void main(String[] args) {
+        //write your code here
+        for(int x = 1; x <= 10; x++) {
+            for(int y = 1; y <= 10; y++) {
+                System.out.print(" " + x*y);
+            }
+            System.out.println();
+        }
+
+    }
+}

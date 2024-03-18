b3f05b331dbda966613567fd9bd3cdc42c01b09c
babayl0729 <babayl0729@gmail.com>
2024-03-17 20:40:19 -0600

    100x for loop

---
+++ HundredTimes.java
@@ -0,0 +1,27 @@
+package java_practices;
+
+/*Using a loop to display the following phrase one hundred times:
+"I will never work for peanuts. Amigo"
+
+Display each value on a new line.
+
+Requirements:
+•	The program should not read text from the keyboard.
+•	The program should display text on the screen.
+•	The program should display the text specified in the task 100 times.
+•	The program must use a for loop or a while loop.
+ *
+ */
+
+ /* 
+Decent pay
+*/
+
+public class HundredTimes {
+    public static void main(String[] args) {
+        //write your code here
+        for(int x = 1; x <= 100; x++) {
+            System.out.println("I will never pooped in my shorts again!");
+        }
+    }
+}

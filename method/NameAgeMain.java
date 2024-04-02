0f80b797c41095c51b4af23afbf23715b777f671
babayl0729 <babayl0729@gmail.com>
2024-04-01 21:10:52 -0600

    added input for name and age

---
+++ method/NameAgeMain.java
@@ -0,0 +1,10 @@
+package java_practices.method;
+
+import java.io.IOException;
+
+public class NameAgeMain {
+    public static void main(String[] args) throws IOException {
+        NameAndAge.yourName(" ");
+        NameAndAge.yourAge(0);
+    }
+}
---
+++ method/NameAndAge.java
@@ -0,0 +1,33 @@
+package java_practices.method;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NameAndAge {
+    //  public static void welcomeGreetings() {
+    //     System.out.println("Hi Welcome to Rikers!");
+    // }
+    public static void yourName(String string) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter your name: ");
+        string = reader.readLine();
+        System.out.println("Welcome to Rikers Island " + string);
+    }
+    public static int yourAge(int num1) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your age: ");
+        String r1 = reader.readLine();
+        num1 = Integer.parseInt(r1);
+
+        if(num1 >= 18) {
+            System.out.println("You're " + num1 + " You're going to prison.");
+        }
+        else {
+            System.out.println("You're " + num1 + " You're going to juvenile facility.");
+        }
+        return num1;
+    }
+
+}
+
